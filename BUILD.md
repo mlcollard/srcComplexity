dfe3454b6ba2c28427cd598ca132fc48cea6b6ed
Michael L. Collard, Ph.D <collard@uakron.edu>
2020-10-28 11:57:02 -0400

    Convert to non-library and non-libxml2 form

--- BUILD.md
+++ BUILD.md
@@ -1,13 +1,11 @@
 # Build for srcComplexity
 
-Dependency on libxml2, the libxml2 library and multiple libxml2 include files.
-
-To clone and build:
+To build and run:
 
 ```console
-git clone git@github.com:UAComputerScience/SEComplexity.git
-mkdir SEComplexity-build
-cd SEComplexity-build
-cmake ../SEComplexity
+mkdir build
+cd build
+cmake ..
 make
+make run
 ```
--- srcMLXPathCount.cpp
+++ srcMLXPathCount.cpp
@@ -8,9 +8,6 @@
 
 #include "memory"
 
-#include <libxml/xpath.h>
-#include <libxml/xpathInternals.h>
-
 /** Count of the result of applying the xpath to the srcML file
  * 
  * XPath expression must result in a numeric, integer result.
@@ -24,57 +21,5 @@
  */
 int srcMLXPathCount(const char* srcMLfile, const char* xpath) {
 
-    // precondition on parameters
-    if (srcMLfile == nullptr || xpath == nullptr)
-        return -2;
-
-    xmlInitParser();
-
-    // open the srcML file with no encoding change and no options
-    xmlDoc* srcMLdoc(xmlReadFile(srcMLfile, nullptr, 0));
-    if (!srcMLdoc) {
-        return -1;
-    }
-
-    // create xpath evaluation context
-    xmlXPathContext* xpathCtx(xmlXPathNewContext(srcMLdoc));
-    if (!xpathCtx) {
-        xmlFreeDoc(srcMLdoc);
-        return -1;
-    }
-
-    // register srcML namespaces
-    if (xmlXPathRegisterNs(xpathCtx, BAD_CAST "src", BAD_CAST "http://www.srcML.org/srcML/src")) {
-        xmlFreeDoc(srcMLdoc);
-        xmlXPathFreeContext(xpathCtx);
-        return -1;
-    }
-    if (xmlXPathRegisterNs(xpathCtx, BAD_CAST "cpp", BAD_CAST "http://www.srcML.org/srcML/cpp")) {
-        xmlFreeDoc(srcMLdoc);
-        xmlXPathFreeContext(xpathCtx);
-        return -1;
-    }
-
-    // evaluate xpath expression
-    xmlXPathObject* xpathResults(xmlXPathEvalExpression(BAD_CAST xpath, xpathCtx));
-    if (!xpathResults) {
-        xmlFreeDoc(srcMLdoc);
-        xmlXPathFreeContext(xpathCtx);
-        return -1;
-    }
-
-    // extract the count
-    int count = xmlXPathCastToNumber(xpathResults);
-    if (count < 0) {
-        xmlFreeDoc(srcMLdoc);
-        xmlXPathFreeContext(xpathCtx);
-        xmlXPathFreeObject(xpathResults);
-        return -1;
-    }
-
-    xmlFreeDoc(srcMLdoc);
-    xmlXPathFreeContext(xpathCtx);
-    xmlXPathFreeObject(xpathResults);
-
-    return count;
-} 
+    return 7;
+}
